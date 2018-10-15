1f7b59cf809c22ff563e4f0cd95273837b928d9b
Dinaol Tadesse <dinaolmelak@gmail.com>
2018-10-15 08:25:15 -0500

    Initial Commit

---
+++ Palindrome/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Palindrome
+//
+//  Created by Dinaol Melak on 10/15/18.
+//  Copyright © 2018 Dinaol Melak. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
